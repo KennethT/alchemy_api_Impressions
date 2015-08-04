103fffde78fb4ec107a45e0d68572fa2527037c4
Kenneth To <kennethkto@gmail.com>
2015-08-04 13:10:49 -0600

    remove general sentiment and clean up controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,33 +1,28 @@
 class WelcomeController < ApplicationController
 
   def index
-    #twitter query
     if params[:search_term1].present?
+      #twitter query and aggregate blob of twitter query
       @tweet1 = $twitter.search("#{params[:search_term1]}", result_type: "recent", lang: "en", geo_enabled: "true").take(100)
-
-      #aggregate blob of twitter query
       @twitterblob = @tweet1.map do |tweet|
         tweet.text
       end.join(', ')
       #search term in uri format
       @search_term_uri = URI.escape(params[:search_term1])
 
-      #sentiment associated with tweets
-      #mixed: 1 == true, type: positive, negative or neutral, score - sentiment strength
-      @sentiment = AlchemyAPI.search(:sentiment_analysis, text: @twitterblob)
-      #entity extraction associated with tweets
+      #AlchemyAPI calls analyzing twitter
       @entity = AlchemyAPI.search(:entity_extraction, text: @twitterblob)
-      #keyword extraction associated with tweets
       @keyword = AlchemyAPI.search(:keyword_extraction, sentiment: "1", maxRetrieve: "40", text: @twitterblob)
 
       #stackoverflow api call
       conn = Faraday.new(:url => 'https://api.stackexchange.com')
-      # post request to watson personality insights api
       stackoverflow_response1 = conn.get do |req|
         req.url "/2.2/search/advanced?order=desc&sort=activity&q=#{@search_term_uri}&site=stackoverflow"
       end
 
-      @stackoverflowresponse = stackoverflow_response1.body
+      @soblob = stackoverflow_response1.body.to_s
+
+      #AlchemyAPI calls analyzing stackoverflow
 
     end
 
