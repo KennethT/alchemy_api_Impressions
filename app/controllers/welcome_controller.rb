6dd8ad078bd2c8a42c8fd5b40b3d78865bb6d0c0
Kenneth To <kennethkto@gmail.com>
2015-08-04 12:03:29 -0600

    add stackoverflow api call to controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
       @twitterblob = @tweet1.map do |tweet|
         tweet.text
       end.join(', ')
-      #twitter blob in uri format
+      #search term in uri format
       @search_term_uri = URI.escape(params[:search_term1])
 
       #sentiment associated with tweets
@@ -19,6 +19,16 @@ class WelcomeController < ApplicationController
       @entity = AlchemyAPI.search(:entity_extraction, text: @twitterblob)
       #keyword extraction associated with tweets
       @keyword = AlchemyAPI.search(:keyword_extraction, sentiment: "1", maxRetrieve: "40", text: @twitterblob)
+
+      #stackoverflow api call
+      conn = Faraday.new(:url => 'https://api.stackexchange.com')
+      # post request to watson personality insights api
+      stackoverflow_response1 = conn.get do |req|
+        req.url "/2.2/search/advanced?order=desc&sort=activity&q=#{@search_term_uri}&site=stackoverflow"
+      end
+
+      @stackoverflowresponse = stackoverflow_response1.body
+
     end
 
   end
